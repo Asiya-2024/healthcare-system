42b3c333fd7181b27ec8c43bc7dc77ce3e039bc9
Asiya Begum Sheik Mohamed <asiyabegum.sheikmohamed@gmail.com>
2024-07-23 14:00:34 -0400

    InPatient class added

---
+++ src/main/java/com/perscholas/java_basics/InPatient.java
@@ -0,0 +1,36 @@
+package com.perscholas.java_basics;
+
+public class InPatient<T extends Patient> {
+    String admissionDate;
+    String roomNumber;
+
+    public InPatient(int patientID,String name,String roomNumber, String admissionDate) {
+        super(patientID,name);
+        this.roomNumber = roomNumber;
+        this.admissionDate = admissionDate;
+    }
+
+    public String getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(String admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "InPatient{" +
+                "admissionDate='" + admissionDate + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                '}';
+    }
+}
