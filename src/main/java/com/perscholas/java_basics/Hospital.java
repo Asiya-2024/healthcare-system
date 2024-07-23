2f21caa6945c106861ce109e7362bc1004bf2e84
Asiya Begum Sheik Mohamed <asiyabegum.sheikmohamed@gmail.com>
2024-07-23 13:33:48 -0400

    Hospital class variables and methods added

--- src/main/java/com/perscholas/java_basics/Hospital.java
+++ src/main/java/com/perscholas/java_basics/Hospital.java
@@ -1,2 +1,23 @@
-package com.perscholas.java_basics;public class Hospital {
+package com.perscholas.java_basics;
+
+import java.util.*;
+
+public class Hospital<T extends Patient> {
+
+    List<T> patients;
+
+    public Hospital(List<T> patients) {
+        this.patients = patients;
+    }
+
+    public List<T> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<T> patients) {
+        this.patients = patients;
+    }
 }
+
+
+
