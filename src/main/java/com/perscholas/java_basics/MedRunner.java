17e10b07abcdea63881982804e6d6a4a5913957b
TorenR <torentremaine@outlook.com>
2024-07-23 13:42:13 -0400

    completed patient class. started MedRunner class

---
+++ src/main/java/com/perscholas/java_basics/MedRunner.java
@@ -0,0 +1,13 @@
+package com.perscholas.java_basics;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MedRunner {
+    public static void main(String[] args) {
+        Patient patient = new Patient();
+        Inpatient inpatient = new Inpatient();
+        OutPatient outPatient = new OutPatient();
+        MedicalRecord medicalRecord = new MedicalRecord();
+        Hospital hospital = new Hospital();
+    }
+}
